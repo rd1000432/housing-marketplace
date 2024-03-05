08c8bef9792d0c9c9258699e22798e4e9517c5a2
Ronan <dineen1@gmail.com>
2024-03-05 18:02:18 +0100

    Google OAuth - login with Google account

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from '../components/OAuth';
 
 function Signin() {
   const [showPassword, setShowPassword] = useState(false);
@@ -90,7 +91,8 @@ function Signin() {
             </div>
           </form>
 
-          {/** Google OAuth **/}
+          <OAuth />
+
           <Link to="/sign-up" className="registerLink">
             Sign Up Instead
           </Link>
